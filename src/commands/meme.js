00b121968daa7d4326fd54a08a6e19b5a85bf55f
Manuel <25lopezm25@gmail.com>
2022-01-09 02:42:08 -0700

    se remplaso got por node-fetch y la url de los memes se guardo en un json

--- src/commands/meme.js
+++ src/commands/meme.js
@@ -1,21 +1,28 @@
 /* eslint-disable indent */
-const got = require('got');
-const Discord = require('discord.js');
+const { memeApiUrl } = require('../../data/memeApi.json');
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
 
 function commandMeme(message) {
-    const embed = new Discord.MessageEmbed();
-    got('https://www.reddit.com/r/memes/random/.json').then(response => {
-        const content = JSON.parse(response.body);
-        const permalink = content[0].data.children[0].permalink;
-        const memeUrl = `https://reddit.com/${permalink}`;
-        const memeImage = content[0].data.children[0].data.url;
-        const memeTitle = content[0].data.children[0].data.title;
-        embed.setTitle(`${memeTitle}`);
-        embed.setColor('RED');
-        embed.setImage(memeImage);
-        embed.setURL(`${memeUrl}`);
-        message.channel.send(embed);
-    });
+    const embed = new MessageEmbed();
+    try {
+        fetch(memeApiUrl)
+            .then(promesaFetch => promesaFetch.json())
+            .then(content => {
+                const permalink = content[0].data.children[0].permalink;
+                const memeUrl = `https://reddit.com/${permalink}`;
+                const memeImage = content[0].data.children[0].data.url;
+                const memeTitle = content[0].data.children[0].data.title;
+                embed.setTitle(`${memeTitle}`);
+                embed.setColor('RED');
+                embed.setImage(memeImage);
+                embed.setURL(`${memeUrl}`);
+                message.channel.send({ embeds: [embed] });
+            });
+    }
+    catch (error) {
+        throw new Error(error.message);
+    }
 }
 
 module.exports = commandMeme;
