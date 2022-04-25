98c518729619ecde193728b1d00d59cf86a5a434
Manuel <25lopezm25@gmail.com>
2022-04-24 19:57:34 -0600

    cambios en la url de una de sus respuestas

--- src/commands/commandRouter.js
+++ src/commands/commandRouter.js
@@ -36,7 +36,7 @@ function loadCommands(client, amogus) {
                 message.channel.send(':kissing_heart::ok_hand:');
             }
             else if(message.content.toLowerCase() === "ahora el manubot tiene su propio servidor corriendo 24/7" && message.author.tag === 'Manuel-2#1186'){
-                await message.reply('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd4m7Z_mhKpIw-whftLymITDcgCOnFEjMZgQ&usqp=CAU');
+                await message.reply('https://ih1.redbubble.net/image.1295771134.0440/st,small,507x507-pad,600x600,f8f8f8.jpg');
                 message.channel.send('LET\'S GOOOOOOOO');
             }
         }
